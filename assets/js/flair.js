156261cc58c83658ee12212b31ee88b6ca297abf
Joel Guilarte <jguilarte01@gmail.com>
2014-08-09 09:54:13 -0400

    Added logic for Panel Learn More Button
    Panel learn more button will now scroll to the tab that has been clicked

--- assets/js/flair.js
+++ assets/js/flair.js
@@ -1,9 +1,18 @@
 $( document ).ready( function () {
-	$( ".hot-panel" ).hover(
-	  function() {
+	$( ".panel-body > a" ).click( function( ) {
+		var input = $( this ).attr( "href" );
+
+		$( '#myTab a[href="' + input +'"]' ).tab('show');
+		$( 'a[data-toggle="tab"]' ).on( 'shown.bs.tab', function ( e ) {
+  			e.target.scrollIntoView();
+  		});
+	});
+
+	$( ".hot-panel" ).hover( 
+		function( ) {
 	  	$( this ).addClass( "panel-hot-active" );
 	  	$( this ).removeClass( "panel-default" );
-	  }, function() {
+	  }, function( ) {
 	    $( this ).removeClass( "panel-hot-active" );
 	    $( this ).addClass( "panel-default" );
 	  }
