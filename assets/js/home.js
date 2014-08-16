485198060a4fc4643c60e62ab4f11b4bc186f1a5
Joel Guilarte <jguilarte01@gmail.com>
2014-08-16 13:49:19 -0400

    Removed javascript that handled navbar used pure css

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,22 +4,4 @@ $( document ).ready( function () {
             top: 500
         }
 	});
-
-	$ ( "#tm-navbar" ).on( "affixed.bs.affix", function () {
-		$( "#tm-navbar" ).css( "left", "0" );
-		$( "#tm-navbar" ).css( "right", "0" );
-		$( "#tm-navbar" ).css( "top", "0" );
-	});
 });
-
-function homeNavBarLoad( ) {
-	var classList = document.getElementById( "tm-navbar" ).getAttribute( "class" ).split( " " );
-
-	for( i = 0; i < classList.length; i++ ) {
-		if ( classList[i] == "affix" ) {
-			$( "#tm-navbar" ).css( "top", "0" );
-			$( "#tm-navbar" ).css( "left", "0" );
-			$( "#tm-navbar" ).css( "right", "0" );
-		}
-	}
-}
