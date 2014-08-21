1fc669d4db8d59ce959be9f2761fcb6239ac5271
Joel Guilarte <jguilarte01@gmail.com>
2014-08-20 23:21:23 -0400

    Improved JS for Panels. Much better now and cleaner it was crap before.

--- assets/js/panels.js
+++ assets/js/panels.js
@@ -2,22 +2,21 @@ $( document ).ready( function () {
 	$( ".panel-body > a" ).click( function( e ) {
 		e.preventDefault( );
 		var id = document.getElementById( $( this ).attr( "href" ).substring( 1 ) ),
-		idClasses = id.getAttribute( "class" ).split( " " );
+		idClasses = id.getAttribute( "class" ).split( " " ),
+		linkTabs = document.getElementById( "myTab" ).children;
 
 		for ( i = 0; i < idClasses.length; i++ ) {
 			if ( idClasses[i] == "active" ) {
-				var linkTabs = document.getElementById( "myTab" ).children;
-				for ( x = 0; x < linkTabs.length; x++ ) {
-					if ( linkTabs[x].children[0].getAttribute( "href" ) == "#" + id.getAttribute( "id" ) ) {
-						linkTabs[x].scrollIntoView();
-					}
-				}
 				break;
 			} else if ( i == idClasses.length - 1 ) {
 				$( '#myTab a[href="#' + id.getAttribute( "id" ) + '"]' ).tab( 'show' );
-				$( 'a[data-toggle="tab"]' ).on( 'shown.bs.tab', function ( e ) {
-		  			e.target.scrollIntoView();
-		  		});
+			}
+		}
+
+		for ( x = 0; x < linkTabs.length; x++ ) {
+			if ( linkTabs[x].children[0].getAttribute( "href" ) == "#" + id.getAttribute( "id" ) ) {
+				linkTabs[x].scrollIntoView();
+				window.scrollBy( 0, -document.getElementsByClassName( "navbar" )[0].scrollHeight )
 			}
 		}		
 	});
